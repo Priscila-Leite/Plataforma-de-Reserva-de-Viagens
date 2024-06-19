d19fe5103a20e8d79985352003f56137832687c3
Priscila Leite <priscilalss@id.uff.br>
2024-06-19 17:39:46 -0300

    inicio

---
+++ Plataforma/src/Main.java
@@ -0,0 +1,7 @@
+import viagem.Cliente;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
---
+++ Plataforma/src/viagem/Cliente.java
@@ -0,0 +1,31 @@
+package viagem;
+
+public class Cliente {
+    String nome, saida, chegada;
+    int estadia;
+    int estrelas;
+    float orcamento;
+
+    public Cliente(String nome,String saida,
+                    String chegada,int estadia,
+                    int estrelas,float orcamento){
+        this.nome = nome;
+        this.saida = saida;
+        this.chegada = chegada;
+        this.estadia = estadia;
+        this.estrelas = estrelas;
+        this.orcamento = orcamento;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "nome: " + nome +
+                ", saida: " + saida +
+                ", chegada: " + chegada +
+                ", estadia: " + estadia + " dias" +
+                ", estrelas: " + estrelas + " estrelas" +
+                ", orcamento: R$ " + orcamento +
+                '}';
+    }
+}
