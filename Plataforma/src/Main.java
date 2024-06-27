5e25b5653c605a37a50068fe688a8b05a204695b
mateusregasi <mateusregasi123@gmail.com>
2024-06-27 00:13:28 -0300

    Criando a classe de Voos

--- Plataforma/src/Main.java
+++ Plataforma/src/Main.java
@@ -1,10 +1,26 @@
 import viagem.Cliente;
+import viagem.Voos;
 import viagem.LeitorCSV;
+import viagem.Csv;
+
+import java.util.function.Consumer;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner; 
 
 public class Main {
     public static void main(String[] args){
-        LeitorCSV ler = new LeitorCSV();
-        List<Cliente> clientes = ler.LeitorClientes("Plataforma/src/csv/clientes.csv");
+        // priscila(args);
+        regasi(args);
+    }
+    public static void priscila(String[] args){
+        // LeitorCSV ler = new LeitorCSV();
+        // List<Cliente> clientes = ler.LeitorClientes("Plataforma/src/csv/clientes.csv");
+    }
+    public static void regasi(String[] args){
+        String pasta = "src/csv/voos.csv";
+        for(Voos s : Voos.get_registros_by_destino(pasta, "RIO")){
+            System.out.printf("%s\n", s);
+        }
     }
 }
---
+++ Plataforma/src/viagem/Csv.java
@@ -0,0 +1,49 @@
+package viagem;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Csv {
+    public String path_uri;
+    public BufferedReader br;
+    public FileReader fr;
+    Csv(String path){
+        try {
+            this.path_uri = new File("").getAbsolutePath() + "/java/trab/Plataforma/" + path;
+            File file = new File(path_uri);
+            this.fr = new FileReader(file);
+            this.br = new BufferedReader(fr);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String[] readLine(){
+        try {
+            String line = this.br.readLine();
+            if (line == null) return null;
+            return line.split(",");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public void close(){
+        try {
+            if (this.br != null) {
+                this.br.close();
+            }
+            if (this.fr != null) {
+                this.fr.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
